8482c2bfe1533227ba165f54b3ac40de1dcd23e5
Salem Gelidi <salemgelidi@gmail.com>
2025-07-25 05:02:58 +0100

    Added XML specification, raw diary example, and outlined functions for reading in a file and parsing XML

--- functions.js
+++ functions.js
@@ -1,3 +1,27 @@
+class project {
+    constructor(name) {
+        this.name = name;
+        this.entries = [];
+    }
+    addEntry(entry) {
+        this.entries.push(entry);
+    }
+    removeEntry(entry) {
+        let index = this.entries.indexOf(entry);
+        this.entries.splice(index, 1);
+    }
+}
+
+class entry {
+    constructor(overview, challenges, newConcepts, thoughts, time) {
+        this.overview = overview;
+        this.challenges = challenges;
+        this.newConcepts = newConcepts;
+        this.thoughts = thoughts;
+        this.time = time;
+    }
+}
+
 function readJournal() {
     document.getElementById("inputFile").addEventListener("change", function (event) {
         const file = event.target.files[0];
@@ -9,4 +33,30 @@ function readJournal() {
         }
         fileReader.readAsText(file);
     })
+}
+
+function parseXML(XMLString) {
+    let journal = [];
+    const parser = new DOMParser();
+    const XMLDoc = parser.parseFromString(XMLString, "application/xml");
+    let DOMProjects = Array.from(XMLDoc.getElementsByTagName("project"));
+
+    DOMProjects.forEach((DOMProject) => {
+        let project = new project(DOMProject.getAttribute("name"));
+        let DOMEntries = Array.from(DOMProject.getElementsByTagName("entry"));
+        
+        DOMEntries.forEach((DOMEntry) => {
+            let overview = getChildTextByTag(DOMEntry, "overview");
+            let challenges = getChildTextByTag(DOMEntry, "challenges");
+            let newConcepts = getChildTextByTag(DOMEntry, "newConcepts");
+            let thoughts = getChildTextByTag(DOMEntry, "thoughts");
+            let time = DOMEntry.getAttribute("time") || "";
+
+            let entry = new entry(overview,challenges,newConcepts,thoughts,time);
+            project.addEntry(entry);            
+        })
+        journal.push(project);
+    });
+
+    return journal;
 }
